7eeb4a881892c55846467ba5279d4e24183faf66
Juste <juste.derk@gmail.com>
2025-10-30 11:51:08 +0100

    finished case site, and changed to php files

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<nav class="side-bar">
+  <ul class="side-bar-navigation">
+    <li><a href="/portfolio.html"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+  </ul>
+  <section class="side-bar-contact-info">
+    <p>Commodo Consequat 23</p>
+    <p><a href="mailto:info@photos.io">info@photos.io</a></p>
+    <p>+46 720 000 100</p>
+  </section>
+</nav>
